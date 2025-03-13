af2b69faee2f636249c4af4b2033856e08982c86
gxost <93872978+gxosty@users.noreply.github.com>
2025-03-13 23:09:21 +0500

    Added `asyncio.hpp` for grouping common headers

---
+++ include/asyncio/asyncio.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "task.h"
+#include "event_loop.h"
+#include "connection.h"
+#include "runner.h"
+#include "schedule_task.h"
+#include "gather.h"
+#include "types.h"
+#include "wait_for.h"
